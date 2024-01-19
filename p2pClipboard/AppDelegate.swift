90451a9e52cf63d0f607b65adbe08af61f27c0c1
gnattu <gnattuoc@me.com>
2024-01-20 01:05:24 +0800

    Initial Commit

---
+++ p2pClipboard/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  p2pClipboard
+//
+//  Created by Gnattu OC on 1/20/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ p2pClipboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  p2pClipboard
+//
+//  Created by Gnattu OC on 1/20/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
